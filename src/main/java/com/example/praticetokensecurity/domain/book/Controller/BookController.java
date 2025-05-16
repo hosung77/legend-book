d6f53a29b0e13f75075870d69be0f579aaf81bd2
xaehub <novxae@gmail.com>
2025-05-16 20:04:25 +0900

    feat: test

--- src/main/java/com/example/praticetokensecurity/domain/book/Controller/BookController.java
+++ src/main/java/com/example/praticetokensecurity/domain/book/Controller/BookController.java
@@ -6,4 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class BookController {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
