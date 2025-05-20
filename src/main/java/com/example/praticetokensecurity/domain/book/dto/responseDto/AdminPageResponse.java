51957adb3db1fd25159d5952ef9f554d5bab97ff
xaehub <novxae@gmail.com>
2025-05-20 12:22:31 +0900

    fix: 등록된 도서 전체 조회 기능 - isDeleted가 false인 도서만 조회되도록 수정

--- src/main/java/com/example/praticetokensecurity/domain/book/dto/responseDto/AdminPageResponse.java
+++ src/main/java/com/example/praticetokensecurity/domain/book/dto/responseDto/AdminPageResponse.java
@@ -1,8 +1,10 @@
 package com.example.praticetokensecurity.domain.book.dto.responseDto;
 
 import java.util.List;
+import java.util.function.Function;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 @Getter
 @AllArgsConstructor
@@ -12,4 +14,12 @@ public class AdminPageResponse<T> {
     private int currentPage;         // 현재 페이지
     private int totalPages;          // 전체 페이지
     private long totalElements;      // 등록된 책 전체 개수
+
+    public <S> AdminPageResponse(Page<S> page, Function<S, T> mapper) {
+        this.content = page.map(mapper).getContent();
+        this.currentPage = page.getNumber() + 1;  // 첫번째 페이지 1부터 시작
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+    }
+
 }
